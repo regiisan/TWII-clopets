40145be3db9007bfc9b2e31e80fd04d7e4669f4d
Martin de Oro <deoromartinignacio@gmail.com>
2025-11-14 14:17:14 -0300

    se crea la vista de usuario

--- front/src/app/modules/auth/auth.routes.ts
+++ front/src/app/modules/auth/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { PerfilComponent } from './pages/perfil/perfil.component'; // <-- IMPORTANTE
 
 export const authRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegisterComponent },
+  { path: 'perfil', component: PerfilComponent },  // <-- NUEVO
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
